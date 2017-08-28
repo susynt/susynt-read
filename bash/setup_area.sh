95b1fbb1939c29e81d2230e546c4cf7aef249869
Daniel Antrim <dantrim@uci.edu>
2017-08-28 07:10:39 -0700

    new cmake based setup scripts

--- bash/setup_area.sh
+++ bash/setup_area.sh
@@ -1,131 +1,172 @@
 #!/bin/bash
 
+##############################################################
+# setup_area
 #
-# This script will checkout SusyNtuple as well as the necessary 
-# packages/dependencies to run over the SusyNt tag requested by 
-# the user
-#
-# Note: This checks out packages from SVN, so you may be asked 
-# to provide your CERN password. Doing "kinit" before running
-# this script is helpful.
+# script to checkout the packages necessary for setting
+# up an area to read susyNt
 #
 # daniel.joseph.antrim@cern.ch
-# July 3 2015
+# August 2017
 #
+#############################################################
 
-#-----------------------------------------------------------
-function print_usage() {
-    echo "Usage:"
-    echo "source setup_area.sh [--dev] [--help]"
-    echo " --dev: checkout the master."
-    echo "        By default checkout the latest stable SusyNtuple tag."
-    echo " --help:   print this message"
+
+function print_usage {
+    echo "------------------------------------------------"
+    echo ""
+    echo "Options:"
+    echo " --stable             Checkout tag n0234(5) branches of SusyNtuple"
+    echo " --master             Checkout master branch of SusyNtuple"
+    echo " --sn                 Set branch/tag to checkout for SusyNtuple"
+    #echo " --skip-patch         Do no tperform patch of SUSYTools"
+    echo " -h|--help            Print this help message"
+    echo ""
+    echo "Example usage:"
+    echo " - Setup the area for stable use:"
+    echo "  $ source setup_area.sh --stable"
+    echo " - Checkout 'cmake' branch of SusyNtuple:"
+    echo "  $ source setup_area.sh --sn cmake"
+    echo " NB A tag/branch for SusyNtuple is required"
+    echo "------------------------------------------------"
 }
-#-----------------------------------------------------------
-function setup_area() {
-    local dev_or_stable="$1"
-    local SVNOFF="svn+ssh://svn.cern.ch/reps/atlasoff/"
-    local SVNPHYS="svn+ssh://svn.cern.ch/reps/atlasphys/"
-    local GIT="https://github.com/" # read-only
 
-    echo "Setting up area for SusyNtuple"
-    date
-
-    if [ "${dev_or_stable}" = "--dev" ]
-    then
-        GIT="git@github.com:" # read+write
-        echo "---------------------------------------------"
-        echo " You are checking out the development"
-        echo " (master) branch of SusyNtuple."
-        tput setaf 1
-        echo " If you mean to read SusyNt's from the n0234 "
-        echo " production, please call this script with"
-        echo " the '--stable' cmd line option."
-        tput sgr0
-        echo "---------------------------------------------"
+function prepare_directories {
+    # make source directory for dumping the code
+    dirname="source/"
+    if [[ -d "$dirname" ]]; then
+        echo "$dirname directory exists"
     else
-        echo "---------------------------------------------"
-        tput setaf 2
-        echo " You are checking out the tag of SusyNtuple  "
-        echo " for the n0234 production of SusyNt."
-        tput sgr0
-        echo "---------------------------------------------"
+        echo "Creating $dirname directory"
+    fi
+    mkdir -p $dirname
+    cp *.patch $dirname
+}
+
+function get_externals {
+    skip_patch=${1}
+    st_tag=${2}
+
+    svnoff="svn+ssh://svn.cern.ch/reps/atlasoff/"
+    svnphys="svn+ssh://svn.cern.ch/reps/atlasphys/"
+
+    dirname="./source/"
+    startdir=${PWD}
+    if [[ -d $dirname ]]; then
+        cd $dirname
+    else
+        echo "ERROR get_externals $dirname directory not found"
+        setup_status=1
+        return 1
     fi
 
-    echo ""
-    echo "Cloning SusyNtuple from ${GIT}susynt/SusyNtuple"
-    git clone ${GIT}susynt/SusyNtuple.git
+    # checkout SUSYTools
+    svncmd="svn co ${svnoff}/PhysicsAnalysis/SUSYPhys/SUSYTools/tags/${st_tag} SUSYTools"
+    $svncmd
+    #svn co ${svnoff}/PhysicsAnalysis/SUSYPhys/SUSYTools/tags/${st_tag} SUSYTools
 
-    if [ "${dev_or_stable}" = "--dev" ]
-    then
-        cd SusyNtuple
-        echo "Checking out the master branch of SusyNtuple"
-        git checkout master
-        cd ..
-    else
-        cd SusyNtuple
-        echo "Checking out the tag SusyNtuple-00-06-00"
-        git checkout SusyNtuple-00-06-00
-        cd ..
+    if [[ ! -d "./SUSYTools/" ]]; then
+        echo "ERROR SUSYTools not checked out properly, SUSYTools/ dir not found"
+        setup_status=1
+        return 1
     fi
 
-    # tags to checkout
-    rootURL="$SVNOFF/PhysicsAnalysis/D3PDTools/RootCore/tags/RootCore-00-04-62"
-    susyURL="$SVNOFF/PhysicsAnalysis/SUSYPhys/SUSYTools/tags/SUSYTools-00-08-64"
+    cd ${startdir}
 
-    echo "Checking out SusyNtuple dependencies"
-    svn co $rootURL RootCore || return || exit
-
-    # Only need minimal SUSYTools
-    echo "Installing minimal SUSYTools"
-    mkdir -p SUSYTools/SUSYTools SUSYTools/Root SUSYTools/cmt SUSYTools/data
-    svn export $susyURL/SUSYTools/SUSYCrossSection.h SUSYTools/SUSYTools/SUSYCrossSection.h
-    svn export $susyURL/Root/SUSYCrossSection.cxx SUSYTools/Root/SUSYCrossSection.cxx
-    svn export $susyURL/cmt/Makefile.RootCore SUSYTools/cmt/Makefile.RootCore
-    svn co $susyURL/data SUSYTools/data || return || exit
-
-    # modify the SUSYTools dependencies
-    sed -i "s/^PACKAGE_DEP.*/PACKAGE_DEP = /" SUSYTools/cmt/Makefile.RootCore
-    sed -i "s/^PACKAGE_REFLEX.*/PACKAGE_REFLEX = /" SUSYTools/cmt/Makefile.RootCore
-    # sed -i "s/^PACKAGE_DEP.*/PACKAGE_DEP = CalibrationDataInterface/" SUSYTools/cmt/Makefile.RootCore
-
-    # patch for 07-82: drop dependecy on PathResolver
-    patch -p0 < SUSYCrossSection.patch
-
-    echo ""
-    echo "Finished."
-    date
 }
-#-----------------------------------------------------------
-function main() {
-    # parse as in
-    # http://stackoverflow.com/questions/192249/how-do-i-parse-command-line-arguments-in-bash
-    local dev_or_stable="--stable"
-    local help=""
-    while [[ $# > 0 ]]
+
+function get_susynt {
+
+    sn_tag=${1}
+    dirname="./source/"
+    startdir=${PWD}
+    if [[ -d $dirname ]]; then
+        cd $dirname
+    else
+        echo "ERROR get_susynt $dirname directory not found"
+        setup_status=1
+        return 1
+    fi
+
+    git clone -b master git@github.com:susynt/SusyNtuple.git SusyNtuple
+
+    if [[ ! -d "./SusyNtuple/" ]]; then
+        echo "ERROR SusyNtuple not checked out properly, SusyNtuple/ dir not found"
+        setup_status=1
+        return 1
+    fi
+
+    cd ./SusyNtuple/
+    git checkout $sn_tag
+    cd -
+    cd ${start_dir}
+
+}
+
+function main {
+
+    sn_tag=""
+    st_tag="SUSYTools-00-08-65"
+    skip_patch=0
+
+    while test $# -gt 0
     do
-        key="$1"
-        case $key in
-            --dev)
-                dev_or_stable="--dev"
+        case $1 in
+            --stable)
+                sn_tag="SusyNtuple-00-06-01" # n0234
                 ;;
-            -h|--help)
-                help=true
+            --master)
+                sn_tag="master"
+                ;;
+            --sn)
+                sn_tag=${2}
+                shift
+                ;;
+            #--skip-patch)
+            #    skip_patch=1
+            #    ;;
+            -h)
+                print_usage
+                return 0
+                ;;
+            --help)
+                print_usage
+                return 0
                 ;;
             *)
-                # unknown option
+                echo "ERROR Invalid argument: $1"
+                return 1
                 ;;
         esac
-        shift # past argument or value
+        shift
     done
 
-    if [[ ${help} ]]
-    then
-        print_usage
-    else
-        setup_area ${dev_or_stable}
+    if [[ $sn_tag == "" ]]; then
+        echo "ERROR SusyNtuple tag is not set"
+        return 1
     fi
+
+    echo "setup_area    Starting -- `date`"
+    echo "setup_area    Checking out SusyNtuple tag : $sn_tag"
+    echo "setup_area    Checking out SUSYTools tag  : $st_tag"
+
+    startdir=${PWD}
+    setup_status=0
+    prepare_directories
+    cd ${startdir}
+    get_externals $skip_patch $st_tag
+    cd ${startdir}
+    if [[ ${setup_status} == 1 ]]; then
+        return 1
+    fi
+    get_susynt $sn_tag
+    cd ${startdir}
+    if [[ ${setup_status} == 1 ]]; then
+        return 1
+    fi
+
+    echo "setup_area    Finished -- `date`"
 }
 
-#-----------------------------------------------------------
+
 main $*
--- bash/setup_release.sh
+++ bash/setup_release.sh
@@ -1,25 +1,163 @@
 #!/bin/bash
 
+#########################################################
+# setup_release
 #
-# This script setups up the release/root version for the area
-# to run over the requested SusyNt tag.
+# script to compile the packages picked up by the
+# setup_area script
 #
 # daniel.joseph.antrim@cern.ch
-# July 3 2015
+# August 2017
 #
+########################################################
 
-rootver=6.04.16-x86_64-slc6-gcc49-opt
-echo ""
-echo "Setting up ROOT ${rootver}"
-lsetup "root ${rootver} --skipConfirm"
+default_release="AnalysisBase,21.2.2,here"
 
-# if rootcore is already set up, clean up the env
-if [ -d ${ROOTCOREDIR} ];
-then
-    source ${ROOTCOREDIR}/scripts/unsetup.sh
-fi
-source RootCore/scripts/setup.sh
-rc find_packages
-rc clean
-rc compile
+function print_usage {
+    echo "------------------------------------------------"
+    echo "setup_release"
+    echo ""
+    echo "Options:"
+    echo " -r|--release             Set the AnalysisBase release"
+    echo "                              > default: $default_release"
+    echo " -c|--compile             Perform full compilation"
+    echo "                              > default: false"
+    echo " -h|--help                Print this help message"
+    echo ""
+    echo " Example usage:"
+    echo " - First time setup and compilation:" 
+    echo "    $ source setup_release.sh --compile"
+    echo " - Area is compiled, but starting from new shell:"
+    echo "    $ source_setup_release.sh"
+    echo "------------------------------------------------"
+}
 
+function setup_release {
+
+    release=${1}
+
+    dirname="./source/"
+    startdir=${PWD}
+    if [[ -d $dirname ]]; then
+        cd $dirname
+    else
+        echo "ERROR setup_release Directory $dirname not found"
+        setup_status=1
+        return 1
+    fi
+
+    echo "Setting up release: $release"
+    export ATLAS_LOCAL_ROOT_BASE="/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase"
+    source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh
+    lsetup "asetup $release"
+    cd $startdir
+
+}
+
+function compile_the_code {
+
+    builddir="./build/"
+    startdir=${PWD}
+    if [[ -d $builddir ]]; then
+        echo "WARNING Removing old build directory"
+        rm -r $builddir
+    fi
+
+    mkdir -p $builddir
+    cd $builddir
+    echo "setup_release    Calling cmake"
+    cmake ../source
+    echo "setup_release    Calling make"
+    make
+    cd $startdir
+
+}
+
+function setup_env {
+
+    builddir="./build"
+    startdir=${PWD}
+    if [[ -d $builddir ]]; then
+        cd $builddir
+        founddir=0
+        for f in *; do
+            if [ $f == *"x86_64"* ]; then
+                cd $f
+                founddir=1
+                if [[ -f "setup.sh" ]]; then
+                    source setup.sh
+                else
+                    echo "WARNING setup.sh file not found in ${PWD}"
+                fi
+            fi
+        done
+        if [[ $founddir == 0 ]]; then
+            echo "WARNING Did not find x86_64-* directory in $builddir"
+        fi
+    else
+        echo "$builddir directory not found, no AnalysisBase_PLATFORM"
+    fi
+    cd $startdir
+}
+
+function main {
+
+    release=$default_release
+    compile=0
+    setup_status=0
+
+    while test $# -gt 0
+    do
+        case $1 in
+            -r)
+                release=${2}
+                shift
+                ;;
+            --release)
+                release=${2}
+                shift
+                ;;
+            -c)
+                compile=1
+                ;;
+            --compile)
+                compile=1
+                ;;
+            -h)
+                print_usage
+                return 0
+                ;;
+            --help)
+                print_usage
+                return 0
+                ;;
+            *)
+                echo "ERROR Invalid argument: $1"
+                return 1
+                ;;
+        esac
+        shift
+    done
+
+    echo "setup_release    Start -- `date`"
+    startdir=${PWD}
+
+    setup_release $release
+    if [[ $setup_status == 1 ]]; then
+        return 1
+    fi
+    if [[ $compile == 1 ]]; then
+        compile_the_code
+    fi
+    if [[ $setup_status == 1 ]]; then
+        return 1
+    fi
+    cd $startdir
+    setup_env
+    cd $startdir
+
+    echo "setup_release    Finished -- `date`"
+
+}
+
+main $*
